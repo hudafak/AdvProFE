2d39e309027af2550eda500180cf162910d4ffa4
hudafak <pm_bball7@hotmail.com>
2022-07-10 10:14:14 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,7 @@
 import { NgModule } from '@angular/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { BrowserModule } from '@angular/platform-browser';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { MenuModule } from 'primeng/menu';
-import { MenubarModule } from 'primeng/menubar';
-import { TableModule } from 'primeng/table';
-import { ChartModule } from 'primeng/chart';
 import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,19 +12,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
 import { MarkerService } from './marker.service';
-import { RatingModule } from 'primeng/rating';
-import { ToastModule } from 'primeng/toast';
-import { CalendarModule } from 'primeng/calendar';
-import { SliderModule } from 'primeng/slider';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { ContextMenuModule } from 'primeng/contextmenu';
-import { DialogModule } from 'primeng/dialog';
-import { DropdownModule } from 'primeng/dropdown';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTableModule } from '@angular/material/table';
+import * as echarts from 'echarts';
+import { NgxEchartsModule } from 'ngx-echarts';
+
 //components
 import { AppComponent } from './app.component';
 
@@ -47,13 +33,6 @@ import { AppComponent } from './app.component';
     MatPaginatorModule,
     BrowserModule,
     BrowserAnimationsModule,
-    SidebarModule,
-    ButtonModule,
-    TabMenuModule,
-    MenuModule,
-    MenubarModule,
-    TableModule,
-    ChartModule,
     AppRoutingModule,
     FlexLayoutModule,
     MatFormFieldModule,
@@ -61,21 +40,14 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    RatingModule,
-    CalendarModule,
-    SliderModule,
-    DialogModule,
-    MultiSelectModule,
-    ContextMenuModule,
-    DropdownModule,
-    ToastModule,
-    InputTextModule,
-    ProgressBarModule,
     HttpClientModule,
     FormsModule,
-    MatTableModule,
     ReactiveFormsModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatTableModule,
+    NgxEchartsModule.forRoot({
+      echarts
+    })
   ],
   providers: [
     MarkerService
